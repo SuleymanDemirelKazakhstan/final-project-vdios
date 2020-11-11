0870d097d2f0b42ea44b7038f89cfd5581e18332
vdios <190103485@stu.sdu.edu.kz>
2020-11-12 00:30:51 +0600

    objects how in js file

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let pizzas = [
     {   "name":"mexican","source":"images\\mexican.jpg","recipe":"Spicy chicken, tomato sauce, bell peppers, red onion, mozzarella, hot jalapeno peppers, tomatoes, salsa sauce","price":"1950"    },
-    {   "name":"arriva","source":"images\\arriva.jpeg","recipe":"Burger sauce, chicken, ranch sauce, chicken chorizo, bell peppers, red onions, mozzarella, tomatoes, garlic", "price":"1750" }
+    {   "name":"arriva","source":"images\\arriva.jpeg","recipe":"Burger sauce, chicken, ranch sauce, chicken chorizo, bell peppers, red onions, mozzarella, tomatoes, garlic", "price":"1750" },
+    {   "name":"cheese","source":"images\\cheese.jpg","recipe":"Large portion of mozzarella, tomato sauce", "price":"1450"    },
+    {   "name":"ham and sheese","source":"images\\ham_and_cheese.jpg","recipe":"Chicken ham, extra mozzarella, alfredo sauce","price":"1450"    },
+    {   "name":"cheeseburder pizza","source":"images\\cheeseburger_pizza.jpg","recipe":"Meat sauce bolognese, mozzarella, red onion, pickles, tomatoes, burger sauce","price":"1750"    },
+    {   "name":"four seasons","source":"images\\four_seasons.jpg","recipe":"Mozzarella, chicken ham, chicken pepperoni, cheese cubes, tomatoes, champignons, tomato sauce, Italian herbs","price":"1750"    },
+    {   "name":"pizza pie","source":"images\\pizza_pie.jpg","recipe":"Pineapples, lingonberries, condensed milk","price":"1550"    }
 ]
 
 const container = document.querySelector('.pizza_block');
