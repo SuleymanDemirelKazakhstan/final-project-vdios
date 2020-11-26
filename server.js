7a386b229a3994f29ed6e2a3369bcfd911988810
vdios <190103485@stu.sdu.edu.kz>
2020-11-26 12:07:20 +0600

    server created

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-    response.send('qwerty');
+    response.send('');
 });
 
 app.listen(3003,()=>console.log('listening to port 3003'));
