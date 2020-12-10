451bdf94439b17ea78b9a2d1320b00b32b038586
vdios <190103485@stu.sdu.edu.kz>
2020-12-10 10:04:53 +0600

    login and registration works fine

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 
+///login///
+
 router.get('/',(request,response)=>{
     response.render('auth_form');
 });
@@ -16,17 +18,46 @@ router.post('/status',(request,response)=>{
                 if(foundres){
                     if(foundres.password == password){
                         request.session.username = username;
-                        status = `welcome ${username}`;
+                        status = `welcome '${username}', let's <a href='/home'>buy</a> some pizza`;
                     }
                     else{
-                        status = `password for username '${username}' is not correct`;
+                        status = `password for username '${username}' is not correct, please <a href='/auth'>login</a> again`;
                     }
                 }
                 else{
-                    status = `username '${username}' doesn't exist in database`;
+                    status = `'${username}' doesn't exist in database, you can <a href='/auth/regist'>register</a> with this username`;
                 }
-
             response.render('status',{status:status});
+        });
+});
+
+//registration///
+
+router.get('/regist',(request,response)=>{
+    response.render('regist_form');
+});
+
+router.post('/regist/status',(request,response)=>{
+    const username = request.body.username;
+    const password = request.body.password;
+    const currentUser = new User({username:username,password:password});
+
+        User.findOne({username:username},'username',(error,foundres)=>{
+            if(error) throw error;
+
+                if(foundres){
+                    response.render('status',{status:`name '${username}' already exists in database, please <a href='/auth/regist'>register</a> again`});
+                }
+                else{
+                    currentUser.save((err)=>{
+                        if(err){
+                            response.render('status',{status:`invalid input format, please <a href='/auth/regist'>register</a> again`});
+                        }
+                        else{
+                            response.render('status',{status:`name '${username}' successfully registered to database, let's <a href='/auth'>login</a>`});
+                        }
+                    });
+                }
         });
 });
 
